d9c98487f4bb77e20a31d220f66eb8bcae97421c
alessia <alessia@Alepicci.station>
2017-03-24 11:39:24 +0100

    Class "Esame"

---
+++ src/it/polito/tdp/libretto/model/TestEsame.java
@@ -0,0 +1,21 @@
+package it.polito.tdp.libretto.model;
+
+import java.time.LocalDate;
+
+public class TestEsame {
+
+	public static void main(String[] args) {
+		Esame tdp = new Esame("03FYZ", "Tecniche di Programmazione", "Fulvio Corno");
+		System.out.println(tdp);
+		Esame ami = new Esame("01QZP","Ambient Intelligence","Fulvio Corno");
+		System.out.println(ami);
+		System.out.print(ami.equals(tdp));
+		
+		tdp.supera(30,LocalDate.now());
+		System.out.println(tdp);
+		tdp.supera(18,LocalDate.now());
+		System.out.println(tdp);
+		
+	}
+
+}
